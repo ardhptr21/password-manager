2af094b828e227b23502b07e81c76b852904334f
ardhptr21 <ardhiputrapradana21@gmail.com>
2021-06-20 20:18:54 +0700

    Make new model Account and migrate 0001.initial

--- management/models.py
+++ management/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    site_name = models.URLField(max_length=255)
+    email = models.EmailField(max_length=255, null=True, blank=True)
+    username = models.CharField(max_length=255, null=True, blank=True)
+    password = models.CharField(max_length=255, null=False, blank=False)
