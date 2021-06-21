14d147a5b80cd29c561c9214b4c0bd547fb1d9f2
ardhptr21 <ardhiputrapradana21@gmail.com>
2021-06-21 09:41:38 +0700

    Create 'create page' management ui

--- management/urls.py
+++ management/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
 urlpatterns = [
-    path('list/', TemplateView.as_view(template_name="management/list.html"), name='list')
+    path('list/', TemplateView.as_view(template_name="management/list.html"), name='list'),
+    path('create/', TemplateView.as_view(template_name="management/create.html"), name='create')
 ]
