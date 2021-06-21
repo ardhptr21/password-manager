73b06b12cdcfc7e5c0af703c7903b7456c197a5a
ardhptr21 <ardhiputrapradana21@gmail.com>
2021-06-21 15:15:11 +0700

    Update default image for Account model, and makemigration 0003.alter_account_image_site

--- management/models.py
+++ management/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 class Account(models.Model):
     image_site = models.ImageField(
-        upload_to="images", default="default.png", blank=True)
+        upload_to="images", default="images/default.png", blank=True)
     site_name = models.URLField(max_length=255)
     email = models.EmailField(max_length=255, null=True, blank=True)
     username = models.CharField(max_length=255, null=True, blank=True)
