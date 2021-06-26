f3922f932778a85f11a317159c367050586e49cd
ardhptr21 <ardhiputrapradana21@gmail.com>
2021-06-26 19:47:59 +0700

    Create configuration settings for production

--- core/settings.py
+++ core/settings.py
@@ -1,3 +1,5 @@
+import os
+import dj_database_url
 from pathlib import Path
 from django.urls import reverse_lazy
 
@@ -9,12 +11,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j(i5t0p%u0+h&^=i_=ku57pqryea+&ei=m4x%zg=719)@vff@f'
+SECRET_KEY = os.environ.get('SECRET_KEY', '=ku57pqryea+&ei=m4x%zg=719)@vff@f')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get(
+    'ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 
 # Application definition
@@ -75,7 +78,8 @@ DATABASES = {
         'PORT': '3306'
     }
 }
-
+if os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -114,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
