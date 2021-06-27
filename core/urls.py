aad66f6fc9bcd94025ff275163084fa321350265
ardhptr21 <ardhiputrapradana21@gmail.com>
2021-06-27 09:12:16 +0700

    add config server media file

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve,
+            {'document_root': settings.MEDIA_ROOT}),
     path('admin/', admin.site.urls),
     path('auth/', include(('auth.urls', 'auth'), namespace='auth')),
     path('management/', include(('management.urls',
